2fcd3d2225234eabe4936173b4515a8a31a0aa0e
Dharaneeshwar <daranip@gmail.com>
2021-03-28 14:50:10 +0530

    Angular Folder-Structure done

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AddproductComponent } from './admin/addproduct/addproduct.component';
+import { AdminNavComponent } from './admin/admin-nav/admin-nav.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { OrderlistComponent } from './admin/orderlist/orderlist.component';
+import { ProductEditComponent } from './admin/product-edit/product-edit.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { UserOrderComponent } from './user-order/user-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AddproductComponent,
+    AdminNavComponent,
+    DashboardComponent,
+    OrderlistComponent,
+    ProductEditComponent,
+    LoginComponent,
+    SignupComponent,
+    CartComponent,
+    HomeComponent,
+    UserOrderComponent
   ],
   imports: [
     BrowserModule,
