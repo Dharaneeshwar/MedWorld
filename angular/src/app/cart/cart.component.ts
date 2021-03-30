4a64d125b0ef0b8e27165adb6b358f8f24a35cc9
PraveenRk4085 <ucs18304@rmd.ac.in>
2021-03-30 12:54:36 +0530

    Adding cart and user-order page

--- angular/src/app/cart/cart.component.ts
+++ angular/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartProduct } from '../model/Cart'
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  product !: Array<CartProduct>;
   constructor() { }
 
   ngOnInit(): void {
+    this.product=new Array<CartProduct>();
+
+    const product1=new CartProduct();
+    product1.name='Paracetamol';
+    product1.price=100;
+    product1.quantity=2;
+
+
+    const product2=new CartProduct();
+    product2.name='Jumbojet';
+    product2.price=10000;
+    product2.quantity=12;
+
+    const product3=new CartProduct();
+    product3.name='Enjami';
+    product3.price=1;
+    product3.quantity=22;
+
+
+    this.product.push(product1);
+    this.product.push(product2);
+    this.product.push(product3);
   }
 
 }
---
+++ angular/src/app/model/Cart.ts
@@ -0,0 +1,5 @@
+export class CartProduct{
+    name !: string; 
+    price !: number;
+    quantity !: number;
+}
