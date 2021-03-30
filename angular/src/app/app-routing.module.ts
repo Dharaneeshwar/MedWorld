3d2f9ed4170f6cb29800de1ef36eda1e989b2707
Vignesh R <43677580+vigneshr1603@users.noreply.github.com>
2021-03-30 10:19:59 +0530

    Adding Login, SignUp and Home pages

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   {path:'admin', component:AdminComponent},
   {path:'addProduct', component:AddproductComponent},
   {path:'admin/orders', component:OrderlistComponent},
+ 
   // {path:'admin/edit', component:AdminComponent},
   // {path:'admin', component:AdminComponent},
   // {path:'admin', component:AdminComponent},
